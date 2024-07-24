c2d47986cd6a2f59f7bf8c21e6bb4929ae6f2ab6
mgrru <ru@fufu.moe>
2024-07-24 14:01:36 +0800

    读取系统默认字体

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,44 @@
 use eframe::egui;
+use egui::{Color32, RichText};
+use fonts::load_font;
+mod fonts;
 
 fn main() -> eframe::Result {
     let options = eframe::NativeOptions::default();
-    let mut text = String::from("mgrru");
-    eframe::run_simple_native("mru_desk", options, move |ctx, _frame| {
+    eframe::run_native(
+        "rupic",
+        options,
+        Box::new(|cc| Ok(Box::new(MyApp::new(cc)))),
+    )
+}
+
+struct MyApp {
+    // text: String,
+}
+
+impl MyApp {
+    fn new(cc: &eframe::CreationContext<'_>) -> Self {
+        load_font(&cc.egui_ctx);
+        Self {
+            // text: "Edit this text field if you want".to_owned(),
+        }
+    }
+}
+
+impl eframe::App for MyApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.label(&text);
-            if ui.button("submit").clicked() {
-                text = "ru".into();
-            }
-            if ui.button("cancel").clicked() {
-                text = "mgrru".into();
-            }
+            if ui
+                .button(
+                    RichText::new("上传")
+                        .color(Color32::RED)
+                        .size(24.0),
+                )
+                .clicked()
+            {};
+            if ui.button("存储").clicked() {};
+            if ui.button("设置").clicked() {};
+            if ui.button("关于").clicked() {};
         });
-    })
+    }
 }
